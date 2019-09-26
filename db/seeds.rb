baf48d7309b1c2e5f28e739d9054b10583ba53a5
jason <jasonedlewis@gmail.com>
2019-09-26 18:43:45 -0400

    Last for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# ==================== USERS ======================== #
+Jack = User.create(username: "Jack", password:"acb123")
+Jill = User.create(username: "Jill", password:"acb123")
+Bill = User.create(username: "Bill", password:"acb123")
+Phil = User.create(username: "Phil", password:"acb123")
+Will = User.create(username: "Will", password:"acb123")
+Eric = User.create(username: "Eric", password:"acb123")
+Derick = User.create(username: "Derick", password:"acb123")
+Ted = User.create(username: "Ted", password:"acb123")
+
+
+
+
+
+
+
+
+
