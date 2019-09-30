530441e55d40da982697c9578ea11ce618f9aeda
jason <jasonedlewis@gmail.com>
2019-09-29 23:30:24 -0400

    added caption column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_170414) do
+ActiveRecord::Schema.define(version: 2019_09_30_005720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_09_27_170414) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "caption"
   end
 
   add_foreign_key "comments", "posts"
