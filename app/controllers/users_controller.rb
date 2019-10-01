98d8397b47b1b9f78bd7e00b544e020bd902b701
jason <jasonedlewis@gmail.com>
2019-10-01 18:29:51 -0400

    backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
         print "What is up, JSON"
         
         user =User.create(user_params)
-        
         if user.valid?
         encoded_token = encode_token(user_payload(user))
          render json: {token: encoded_token}
