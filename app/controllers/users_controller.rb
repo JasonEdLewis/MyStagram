306c62bde677656e21641f31da1009b930b89e61
jason <jasonedlewis@gmail.com>
2019-10-01 14:20:43 -0400

    modified users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,11 @@ class UsersController < ApplicationController
 
 
     def create
+        print "What is up, JSON"
+        
         user =User.create(user_params)
+        
         if user.valid?
-
         encoded_token = encode_token(user_payload(user))
          render json: {token: encoded_token}
 
